74905671f2cbf1efad038a98fba5e5f74ef30add
Yevhen <73534149+Krenichniy@users.noreply.github.com>
2025-09-30 09:16:16 +0300

    done

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,8 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import Stats from "three/addons/libs/stats.module.js";
-import { GUI } from "dat.gui";
 
-const sceneA = new THREE.Scene();
-const sceneB = new THREE.Scene();
-const sceneC = new THREE.Scene();
-sceneA.background = new THREE.Color(0x123456);
-sceneB.background = new THREE.TextureLoader().load(
-    "https://sbcode.net/img/grid.png"
-);
-sceneC.background = new THREE.CubeTextureLoader()
-    .setPath("https://sbcode.net/img/")
-    .load(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]);
-// scene.backgroundBlurriness = 0.2;
+const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -34,58 +23,36 @@ window.addEventListener("resize", () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener("change", function () {
+    renderer.render(scene, camera);
+});
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
-sceneA.add(cube);
+scene.add(cube);
 
 const stats = new Stats();
-// stats.showPanel(1);
 document.body.appendChild(stats.dom);
 
-const gui = new GUI();
+// const clock = new THREE.Clock();
+// let delta;
+// function animate() {
+//     requestAnimationFrame(animate);
 
-const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
-cubeFolder.open(); // open folder by default
+//     delta = clock.getDelta();
+//     cube.rotation.x += delta;
+//     cube.rotation.y += delta;
+//     // cube.rotation.x += 0.01;
+//     // cube.rotation.y += 0.01;
 
-let actualScene = sceneA;
-const setScene = {
-    sceneA: function () {
-        actualScene = sceneA;
-    },
-    sceneB: function () {
-        actualScene = sceneB;
-    },
-    sceneC: function () {
-        actualScene = sceneC;
-    },
-};
-gui.add(setScene, "sceneA").name("Scene A");
+//     renderer.render(scene, camera);
 
-gui.add(setScene, "sceneB").name("Scene B");
+//     stats.update();
+// }
 
-gui.add(setScene, "sceneC").name("Scene C");
+// animate();
 
-const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.position, "z", 0, 20);
-cameraFolder.open();
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    // stats.begin();
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    // stats.end();
-
-    renderer.render(actualScene, camera);
-    stats.update();
-}
-
-animate();
+renderer.render(scene, camera);
